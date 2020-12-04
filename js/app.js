bb822972f7a3c0bc5a6ce6ce39206b2a229e9ef6
Vasim Shaikh <vasim.shaikh-tm@partner.import.io>
2020-12-04 17:21:58 +0530

    ex05: wire div to show output

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 var btn = document.querySelector('#btn');
 var inputText = document.querySelector('#inputText');
+var output = document.querySelector('#output');
 btn.addEventListener('click',()=> {
     var textInput = inputText.value;
+    output.innerText =  'you just written text: '+textInput;
     console.log(textInput)
 });
