de4b1f6addf74d1415ef4db5701e6a58994c8735
Vasim Shaikh <vasim.shaikh-tm@partner.import.io>
2020-12-04 17:39:31 +0530

    ex08#ex09: done

--- js/app.js
+++ js/app.js
@@ -2,7 +2,9 @@ var btn = document.querySelector('#btn');
 var inputText = document.querySelector('#inputText');
 var output = document.querySelector('#output');
 btn.addEventListener('click',()=> {
+    output.innerText = "";
     var textInput = inputText.value;
-    output.innerText =  'you just written text: '+textInput;
-    console.log(textInput)
+    fetch(`https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json?text=${textInput}`).then(res => res.json()).then(data => {
+        output.innerText =  `went to server and returned: ${data.contents.text} \n Got translation also: ${data.contents.translated}`;
+    });
 });
