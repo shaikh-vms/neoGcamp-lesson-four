999e027bbf8512fdd5362f270c2975ea9117706b
Vasim Shaikh <vasim.shaikh-tm@partner.import.io>
2020-12-06 22:44:40 +0530

    Update: Styling done

--- js/app.js
+++ js/app.js
@@ -1,12 +1,15 @@
-var btn = document.querySelector('#btn');
-var inputText = document.querySelector('#inputText');
-var output = document.querySelector('#output');
+var btn = document.querySelector("#btn");
+var inputText = document.querySelector("#inputText");
+var output = document.querySelector("#output");
+
+var url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
 btn.addEventListener('click',()=> {
-    output.innerText = "";
+    console.log("button clicked");
     var textInput = inputText.value;
-    fetch(`https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json?text=${textInput}`).then(res => res.json()).then(data => {
-        output.innerText =  `went to server and returned: ${data.contents.text} \n Got translation also: ${data.contents.translated}`;
-    }).catch(()=>{
-        alert('Sorry :( something is wrong with server');
-    });
+    fetch(`https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json?text=${textInput}`)
+        .then(res => res.json())
+        .then(data => {
+            output.innerText =  `went to server and returned: ${data.contents.text} \n Got translation also: ${data.contents.translated}`;
+        })
+        .catch(() => alert('Sorry :( something is wrong with server'))
 });
