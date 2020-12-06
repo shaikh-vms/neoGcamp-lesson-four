7a7ba47a70494614eac2831acc947a3a8efcee66
Vasim Shaikh <vasim.shaikh-tm@partner.import.io>
2020-12-06 22:56:04 +0530

    ex11: prod api updated

--- js/app.js
+++ js/app.js
@@ -2,14 +2,14 @@ var btn = document.querySelector("#btn");
 var inputText = document.querySelector("#inputText");
 var output = document.querySelector("#output");
 
-var url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+var url = "https://api.funtranslations.com/translate/minion.json"
 btn.addEventListener('click',()=> {
     console.log("button clicked");
     var textInput = inputText.value;
-    fetch(`https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json?text=${textInput}`)
+    fetch(`${url}?text=${textInput}`)
         .then(res => res.json())
         .then(data => {
-            output.innerText =  `went to server and returned: ${data.contents.text} \n Got translation also: ${data.contents.translated}`;
+            output.innerText =  data.contents.translated;
         })
         .catch(() => alert('Sorry :( something is wrong with server'))
 });
